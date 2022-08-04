8a8ae684e983e26f34f0b8c2a803b3c0754b5083
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-04 15:37:21 +0100

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import * as VueRouter from 'vue-router'
+
+const routes = []
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+})
+
+export default router
