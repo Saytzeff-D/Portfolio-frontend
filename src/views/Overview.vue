cfebf28644e0f28d864adba010b14a93f459009b
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-05 23:21:08 +0100

    initialised overview component

---
+++ src/views/Overview.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container-fluid">
+        <div class="row w-100 mx-0">
+            <div class="col-lg-3 col-md-6 p-2">
+                <div class="rounded-lg shadow-lg py-5 w3-teal mt-5">
+                    <p class="text-center py-5 h1">
+                        Profile
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 p-2">
+                <div class="rounded-lg shadow-lg py-5 w3-pink mt-5">
+                    <p class="text-center py-5 h1">
+                        Resume
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 p-2">
+                <div class="rounded-lg shadow-lg py-5 w3-blue mt-5">
+                    <p class="text-center py-5 h1">
+                        Project
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 p-2">
+                <div class="rounded-lg shadow-lg py-5 w3-orange mt-5">
+                    <p class="text-center py-5 h1">
+                        Testimonials
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'OverView'
+    }
+</script>
+
+<style>
+    .rounded-lg{
+        border-radius: 30% !important;
+    }
+</style>
