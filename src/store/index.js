dd32f94217b58f2bf9f0cabc08cb80c17746931e
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-09 04:24:23 +0100

    implement getters, mutations and actions in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,39 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 const store = createStore({
     state() {
-        return{}
+        return{
+            serverUrl: 'http://localhost:2020/portfolio/',
+            project: [],
+            projectError: ''
+        }
     },
-    mutations: {}
+    mutations: {
+        allProject(state, project){
+            state.project = project
+        },
+        projectError (state, payload){
+            state.projectError = payload
+        }
+    },
+    getters: {
+        allMyProjects(state){
+            return state.project
+        },
+        projectError (state){
+            return state.projectError
+        }
+    },
+    actions: {
+        getAllProject(context){
+            axios.get(`${context.state.serverUrl}allProject`).then((res)=>{
+                context.commit('allProject', res.data.project)
+            }).catch((err)=>{
+                context.commit('projectError', err.name)
+            })
+        }
+    }
 })
 
 export default store
