1a45bab40626653417c546d8ff624eb66dbc60a7
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-10 01:17:34 +0100

    changed v-model name

---
+++ src/views/SiteImg.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-row">
+        <div class="form-group col-md-3">
+                <label class="font-weight-bold">Portfolio Banner</label>
+                <input type="file" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+                <label class="font-weight-bold">Your Picture</label>
+                <input type="file" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+                <label class="font-weight-bold">Footer Banner</label>
+                <input type="file" class="form-control" />
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SiteImg'
+}
+</script>
+
+<style>
+
+</style>
