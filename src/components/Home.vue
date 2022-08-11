fe3c3b270f8df32ce9e469aa82c731d579141fe3
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-11 02:51:37 +0100

    I added header, navbar and aboutme component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="w3-top">
+    <div>
         <Header />
+        <NavBar />
+        <AboutMe />
     </div>
 </template>
 
 <script>
 import Header from '@/pages/Header.vue';
+import NavBar from '@/pages/NavBar.vue';
+import AboutMe from '@/pages/About.vue';
 export default {
     name: "MyHome",
-    components: {Header },
+    components: { Header, NavBar, AboutMe },
     mounted(){
-        this.$store.dispatch('myProfile')
+        // this.$store.dispatch('myProfile')
     }
 }
 </script>
