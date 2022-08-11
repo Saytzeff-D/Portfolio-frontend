81fb3bc48702b5cc57f066add90a25634a7fc0f4
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-11 02:51:06 +0100

    working on this

---
+++ src/pages/MyResume.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'MyResume',
+    computed: {
+        profile(){
+            return this.$store.getters.fetchProfile
+        }
+    }
+}
+</script>
