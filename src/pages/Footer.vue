81984482bc5c3997bb977ed8cea06d65eb70756e
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-13 03:24:23 +0100

    completed footer

---
+++ src/pages/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center py-3 h6 text-danger">
+        &copy; 2022 | Saytzeff-D | Full Stack Developer | DavifyRx
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PageFooter'
+}
+</script>
