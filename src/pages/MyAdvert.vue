f7f9b38f80fb18036b3837605db78a665b824f58
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-13 03:24:38 +0100

    still working on this

---
+++ src/pages/MyAdvert.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <section class="bg-dark text-center">
+            <p class="font-weight-bolder text-white h2 py-4">I'm available for freelance</p>
+        <div class="section container">
+            <div class="row text-center">
+                <div class="col-md-6 col-lg-3">
+                    <div class="row ">
+                        <div class="col-5 text-right text-light border-right py-3">
+                            <div class="m-auto"><i class="fa fa-github icon-xl"></i></div>
+                        </div>
+                        <div class="col-7 text-left py-3">
+                            <h1 class="text-danger font-weight-bold font40">500</h1>
+                            <p class="text-light mb-1">Hours Worked</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="row">
+                        <div class="col-5 text-right text-light border-right py-3">
+                            <div class="m-auto"><i class="fa fa-send-back icon-xl"></i></div>
+                        </div>
+                        <div class="col-7 text-left py-3">
+                            <h1 class="text-danger font-weight-bold font40">50K</h1>
+                            <p class="text-light mb-1">Project Finished</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="row">
+                        <div class="col-5 text-right text-light border-right py-3">
+                            <div class="m-auto"><i class="fa fa-face-smile icon-xl"></i></div>
+                        </div>
+                        <div class="col-7 text-left py-3">
+                            <h1 class="text-danger font-weight-bold font40">200K</h1>
+                            <p class="text-light mb-1">Happy Clients</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="row">
+                        <div class="col-5 text-right text-light border-right py-3">
+                            <div class="m-auto"><i class="fa fa-heart-broken icon-xl"></i></div>
+                        </div>
+                        <div class="col-7 text-left py-3">
+                            <h1 class="text-danger font-weight-bold font40">2k</h1>
+                            <p class="text-light mb-1">Coffee Drinked</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="d-flex justify-content-center py-5">
+                <button class="btn btn-danger font-weight-bolder px-5">Hire Me</button>
+            </div>
+    </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyAdvert'
+}
+</script>
+
+<style scoped>
+.section {
+  /* padding-top: 80px; */
+}
+.icon-xl {
+  font-size: 65px;
+}
+</style>
