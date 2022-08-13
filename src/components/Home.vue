4bdb836fd19ec9960f6a90b999ea9488918b7d95
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-13 03:22:36 +0100

    footer, myadvert and myresume done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <Header />
-        <NavBar />
-        <AboutMe />
+        <Header :profile="profile" />
+        <NavBar :profile="profile" />
+        <AboutMe :profile="profile" :expert="expert" />
+        <MyResume :expert="expert" :education="education" :skills="skills" :language="language" />
+        <MyAdvert />
+        <Footer />
     </div>
 </template>
 
@@ -10,11 +13,35 @@
 import Header from '@/pages/Header.vue';
 import NavBar from '@/pages/NavBar.vue';
 import AboutMe from '@/pages/About.vue';
+import MyResume from '@/pages/MyResume.vue';
+import MyAdvert from '@/pages/MyAdvert.vue';
+import Footer from '@/pages/Footer.vue'
 export default {
     name: "MyHome",
-    components: { Header, NavBar, AboutMe },
+    components: { Header, NavBar, AboutMe, MyResume, MyAdvert, Footer },
+    computed: {
+        profile(){
+            return this.$store.getters.fetchProfile
+        },
+        expert(){
+            return this.$store.getters.getExpertiseTray
+        },
+        education(){
+            return this.$store.getters.getEduTray
+        },
+        skills(){
+            return this.$store.getters.getSkill
+        },
+        language(){
+            return this.$store.getters.getLanguage
+        }
+    },
     mounted(){
-        // this.$store.dispatch('myProfile')
+        this.$store.dispatch('myProfile')
+        this.$store.dispatch('getExpertise')
+        this.$store.dispatch('fetchEduArray')
+        this.$store.dispatch('getSkill')
+        this.$store.dispatch('getLang')
     }
 }
 </script>
